6500b5f5732a419e44483789fb617380c5d26e74
ik <ik@ik-PC>
2019-09-11 22:58:55 +0530

    change to String array to Arraylist

--- SimpleApp/src/com/it/HelloEarth.java
+++ SimpleApp/src/com/it/HelloEarth.java
@@ -1,11 +1,19 @@
 package com.it;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloEarth {
-	static String[] names = { "indrakeet", "ashish", "abhishek", "chhavi", "shrashti" };
-
+	//static String[] names = { "indrakeet", "ashish", "abhishek", "chhavi", "shrashti" };
+ static List<String> list;
 	public static void main(String[] args) {
-
-		for (String name : names) {
+       list=new ArrayList<>();
+       list.add("indrajeet");
+       list.add("ashish");
+       list.add("abhishek");
+       list.add("chhavi");
+       list.add("shrashti");
+		for (String name : list) {
 			System.out.println(name);
 		}
 	}
